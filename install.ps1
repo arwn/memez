7bc5eadad0bb79cb4619e56f0067326dc34bfddd
arwn <windham.aren@protonmail.com>
2019-05-24 15:41:56 -0700

    now installs the task for the times/days correctly

--- install.ps1
+++ install.ps1
@@ -6,6 +6,8 @@ $filepath = 'C:\memez.exe'
 icacls $filepath /setowner admin
 
 # create  task
-$trigger = New-ScheduledTaskTrigger -AtLogOn
+$trigger = New-ScheduledTaskTrigger -At 11pm -Weekly -DaysOfWeek monday, tuesday, wednesday, thursday, saturday, sunday
+$triggerFriday = New-ScheduledTaskTrigger -At 12pm -Weekly -DaysOfWeek friday
 $action = New-ScheduledTaskAction -execute $filepath
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "memez" -Description "Anti gamer script"
+Register-ScheduledTask -Action $action -Trigger $triggerFriday -TaskName "memezf" -Description "Anti gamer script, on friday"
