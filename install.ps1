60fe9194d8383d996ecba6ba0079b6f9ee607ab6
AREN WINDHAM <18649645+arwn@users.noreply.github.com>
2019-07-10 15:07:04 -0700

    added target user variable

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
 ## This script deploy memez as a service
-# set path to exe
+$username = "42" # change username to the target user
+# don't change anything below here
+
 $filepath = 'C:\memez.exe'
 
 # chown file
@@ -9,5 +11,5 @@ icacls $filepath /setowner admin
 $trigger = New-ScheduledTaskTrigger -At 11pm -Weekly -DaysOfWeek monday, tuesday, wednesday, thursday, saturday, sunday
 $triggerFriday = New-ScheduledTaskTrigger -At 12pm -Weekly -DaysOfWeek friday
 $action = New-ScheduledTaskAction -execute $filepath
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "memez" -Description "Anti gamer script"
-Register-ScheduledTask -Action $action -Trigger $triggerFriday -TaskName "memezf" -Description "Anti gamer script, on friday"
+Register-ScheduledTask -Action $action -Trigger $trigger -User $username -TaskName "memez" -Description "Anti gamer script"
+Register-ScheduledTask -Action $action -Trigger $triggerFriday -User $username -TaskName "memezf" -Description "Anti gamer script, on friday"
